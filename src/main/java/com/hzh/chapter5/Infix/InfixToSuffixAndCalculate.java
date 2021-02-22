fbe247180f12200d2946175e9d3dcda1cadd3a21
huangzhenhui <huangzhenhui>
2021-02-22 17:30:37 +0800

    '中缀表达式计算'

--- src/main/java/com/hzh/chapter5/Infix/InfixToSuffixAndCalculate.java
+++ src/main/java/com/hzh/chapter5/Infix/InfixToSuffixAndCalculate.java
@@ -3,13 +3,97 @@ package com.hzh.chapter5.Infix;
 import com.hzh.chapter5.stackarray.StackArray;
 import com.hzh.chapter5.stackarray.StackArray2;
 
+import java.util.Stack;
+
 /**
  * @description: 中缀表达式
- * 来自@https://blog.csdn.net/sgbfblog/article/details/8001651?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.control&dist_request_id=ecb97cc5-3727-45a3-899a-aa4a67d70d13&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.control
+ * 来自 https://blog.csdn.net/sgbfblog/article/details/8001651?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.control&dist_request_id=ecb97cc5-3727-45a3-899a-aa4a67d70d13&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.control
  * @Author huangzhenhui
  * @Date 2021/2/22 11:34
  */
 public class InfixToSuffixAndCalculate {
+
+    /**
+     * 来自 https://blog.csdn.net/sinat_27908213/article/details/80273557
+     * ① 准备一个数字栈和一个符号栈
+     * ② 从左到右遍历中缀表达式。如果遇到数字，入数字栈。
+     * ③ 如果遇到符号（四个运算符以及括号），跟前面的“中缀表达式转后缀表达式”过程一样，对符号栈进行处理。处理过程中，
+     *  对每一个出栈的运算符：+ - * /，根据“计算后缀表达式”的方法，计算结果（跟数字栈配合）。
+     *  如果遍历完中缀表达式后符号栈还非空，就继续出符号栈的运算符，计算，直到符号栈为空。最后数字栈剩下的数字就是结果。
+     * @return
+     */
+    public int calCulateDirect(String infix) {
+        Stack<Integer> numStack = new Stack<>();
+        Stack<Character> symbolStack = new Stack<>();
+
+        for(int i = 0; i < infix.length(); i++) {
+            char c = infix.charAt(i);
+            // 1 数字
+            if (c >= '0' && c <= '9') {
+                numStack.push(c - '0');
+                continue;
+            }
+            // 2 符号
+            if (c == '+' || c == '-' || c == '*' || c == '/') {
+                while (!symbolStack.empty()) {
+                    if (getPriority(c) > getPriority(symbolStack.peek()) || symbolStack.peek() == '(') {  // 校验和上一位的优先级, 从栈中弹出元素直到遇到发现更低优先级的元素(或者栈为空)为止
+                        symbolStack.push(c);
+                        break;
+                    } else {
+                        // 符号出栈, 计算
+                        char pop = symbolStack.pop();
+                        numStack.push(calculateBySymbol(numStack.pop(), numStack.pop(), pop));
+                    }
+                }
+                if (symbolStack.empty()) { // 如果最后还是空或者一开始就为空, 压栈
+                    symbolStack.push(c);
+                }
+            }
+            // 3 ( 优先级最大直接压栈
+            if(c == '(') {
+                symbolStack.push(c);
+                continue;
+            }
+            // 4 只有遇到)时, (才会弹出, 中间的全部出栈计算
+            if (c == ')') {
+                while (!symbolStack.empty()) {
+                    char pop = symbolStack.pop();
+                    if (pop != '(') {
+                        // 符号出栈, 计算
+                        int result = calculateBySymbol(numStack.pop(), numStack.pop(), pop);
+                        numStack.push(result);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        // 5 如果符号栈还有, 继续出栈计算
+        while (!symbolStack.empty()) {
+            numStack.push(calculateBySymbol(numStack.pop(), numStack.pop(), symbolStack.pop()));
+        }
+        return numStack.pop();
+    }
+
+    /**
+     * 假定公式格式正确
+     * @param num2
+     * @param num1
+     * @param c
+     * @return
+     */
+    private int calculateBySymbol(Integer num2, Integer num1, char c) {
+        if(c == '+') {
+            return num1 + num2;
+        } else if(c == '-') {
+            return num1 - num2;
+        } else if(c == '*') {
+            return num1 * num2;
+        } else if(c == '/') {
+            return num1 / num2;
+        }
+        throw new RuntimeException("???");
+    }
 
     /**
      * 中缀表达式转后缀， 计算后缀表达式(逆波兰表达式)
@@ -36,6 +120,7 @@ public class InfixToSuffixAndCalculate {
         for (int i = 0; i < suffix.length(); i++) {
             char c = suffix.charAt(i);
             if (c >= '0' && c <= '9') {
+                // 注意, char数字转int可以用 - '0'
                 stack.push(c - '0');
             } else {
                 int num2 = stack.pop();
@@ -147,7 +232,9 @@ public class InfixToSuffixAndCalculate {
 
     public static void main(String[] args) {
         InfixToSuffixAndCalculate infixToSuffixAndCalculate = new InfixToSuffixAndCalculate();
-        int calculate = infixToSuffixAndCalculate.calculate("3+2*6-2");
-        System.out.println(calculate);
+        int calculate1 = infixToSuffixAndCalculate.calculate("(1+(2-3)*4+4/2+2+2*(6/2)+9-2+6/3)*8/2");
+        int calculate2 = infixToSuffixAndCalculate.calCulateDirect("(1+(2-3)*4+4/2+2+2*(6/2)+9-2+6/3)*8/2");
+        System.out.println(calculate1);
+        System.out.println(calculate2);
     }
 }
